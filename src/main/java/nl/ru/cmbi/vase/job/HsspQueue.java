b490c86042a2d6a7558d5f586c7371fcf7931c3b
cbaakman <c.baakman@umcn.nl>
2014-06-28 21:06:26 +0200

    BUGFIX: handle missing cache dir
    fixed a nullpointer exception that only appeared when the cache
    directory name was invalid.

--- src/main/java/nl/ru/cmbi/vase/job/HsspQueue.java
+++ src/main/java/nl/ru/cmbi/vase/job/HsspQueue.java
@@ -15,76 +15,78 @@ import org.slf4j.LoggerFactory;
 import nl.ru.cmbi.vase.tools.util.Config;
 
 public class HsspQueue {
-	
+
 	private static Logger log = LoggerFactory.getLogger(HsspQueue.class);
-	
+
 	private static final File cacheDir = new File(Config.properties.getProperty("hsspcache"));
-	
+
 
 	private ThreadPoolExecutor executor;
-	
+
 	public HsspQueue(int nThreads) {
-		
+
 		executor = new ThreadPoolExecutor(nThreads, nThreads,
 	        0L, TimeUnit.MILLISECONDS,
 	        new LinkedBlockingQueue<Runnable>());
-		
+
 		loadCache();
 	}
-	
+
 	private Map<String,HsspJob> jobs = new HashMap<String,HsspJob>();
 
 	private void loadCache() {
-		
-		for(File file : cacheDir.listFiles(pdbfilter)) {
 
-			HsspJob job = new HsspJob(file);
-			
-			jobs.put(job.getUUID().toString(), job);
-			
-			if( !job.getHsspFile().isFile() && !job.getErrorFile().isFile() ) {				
-			
-				executor.execute(job);				
-			}
+		if(cacheDir!=null && cacheDir.isDirectory()) {
+            for(File file : cacheDir.listFiles(pdbfilter)) {
+
+                HsspJob job = new HsspJob(file);
+
+                jobs.put(job.getUUID().toString(), job);
+
+                if( !job.getHsspFile().isFile() && !job.getErrorFile().isFile() ) {
+
+                    executor.execute(job);
+                }
+            }
 		}
 	}
-	
+
 	public String submit(String pdbContents) {
-		
+
 		HsspJob job = new HsspJob(pdbContents);
-		
+
 		if(!jobs.containsKey(job.getUUID().toString())) {
-		
+
 			jobs.put(job.getUUID().toString(), job);
 
 			executor.execute(job);
 		}
-		
+
 		return job.getUUID().toString();
 	}
-	
+
 	public JobStatus getStatus(String id) {
-		
+
 		if(jobs.containsKey(id)) {
-			
+
 			return jobs.get(id).getStatus();
 		}
-		
+
 		else return JobStatus.UNKNOWN;
 	}
 
 	private static final FilenameFilter pdbfilter = new FilenameFilter() {
-		
+
         public boolean accept(File directory, String fileName) {
-        	
+
             return fileName.endsWith(".pdb.gz");
         }
 	};
-	
+
 	private static final FilenameFilter hsspfilter = new FilenameFilter() {
-		
+
         public boolean accept(File directory, String fileName) {
-        	
+
             return fileName.endsWith(".hssp.bz2");
         }
 	};
