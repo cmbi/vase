246b299f6d0ecd87b00510d4742f1a55d15498f3
cbaakman <c.baakman@umcn.nl>
2014-06-28 21:30:56 +0200

    locale fix for data table
    This locale fix makes sure the numerical data is displayed in english
    locale. Otherwise the application might have trouble running on machines
    with a different language.

--- src/main/java/nl/ru/cmbi/vase/data/TableData.java
+++ src/main/java/nl/ru/cmbi/vase/data/TableData.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -314,7 +315,7 @@ public class TableData {
 		
 		else if(ColumnDataType.DOUBLE.matchesByClass(value))
 			
-			return String.format("%.2f", matrix.get(rowIndex).get(columnIndex));
+			return String.format(Locale.ENGLISH,"%.2f", matrix.get(rowIndex).get(columnIndex));
 		else
 			return matrix.get(rowIndex).get(columnIndex).toString();
 	}
