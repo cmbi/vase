7c5d8a9fb767f82219ec4d9793e69432937d9105
Coos Baakman <cbaakman@gmail.com>
2014-07-14 13:35:47 +0200

    moved stockholm and rcsb url to utils class

---
+++ src/main/java/nl/ru/cmbi/vase/tools/util/Utils.java
@@ -0,0 +1,63 @@
+package nl.ru.cmbi.vase.tools.util;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class Utils {
+	
+	public static final String stockholmLocationURL = "ftp://ftp.cmbi.ru.nl/pub/molbio/data/hssp3",
+								rcsbLocationURL = "http://www.rcsb.org/pdb/files";
+	
+	
+	public static URL getStockholmURL( String pdbid ) throws MalformedURLException {
+		
+		return new URL(String.format("%s/%s.hssp.bz2",stockholmLocationURL, pdbid));
+	}
+	
+	public static URL getRcsbURL( String pdbid ) throws MalformedURLException {
+		
+		return new URL(String.format("%s/%s.pdb",rcsbLocationURL, pdbid));
+	}
+	
+	public static <T extends Number> T max(List<T> numbers) {
+		
+		T highest = numbers.get(0);
+		for(T n : numbers) {
+			
+			if(n.doubleValue()>highest.doubleValue()) {
+				
+				highest=n;
+			}
+		}
+		return highest;
+	}
+	public static <T extends Number> T min(List<T> numbers) {
+		
+		T lowest = numbers.get(0);
+		for(T n : numbers) {
+			
+			if(n.doubleValue()<lowest.doubleValue()) {
+				
+				lowest=n;
+			}
+		}
+		return lowest;
+	}
+	
+	public static List<Integer> listRange(int start,int end) {
+
+		List<Integer> is = new ArrayList<Integer>();
+		for(int i=start; i<end; i++){
+			is.add(i);
+		}
+		return is;
+	}
+}
