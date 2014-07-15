357224779cee3a53df209d92448919600f6f5b78
Coos Baakman <cbaakman@gmail.com>
2014-07-15 16:13:14 +0200

    now extends basepage

--- src/main/java/nl/ru/cmbi/vase/web/page/ErrorPage.java
+++ src/main/java/nl/ru/cmbi/vase/web/page/ErrorPage.java
@@ -3,10 +3,11 @@ package nl.ru.cmbi.vase.web.page;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
-public class ErrorPage extends WebPage {
+public class ErrorPage extends BasePage {
 
 	public ErrorPage(String message) {
 	
+		setPageTitle("ERROR");
 		add(new Label("message",message));
 	}
 }
