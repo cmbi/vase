c902d8306587f143049fd0fca314d29bd156696b
Coos Baakman <cbaakman@gmail.com>
2014-07-16 17:55:16 +0200

    Added some contents to the home and about pages

--- src/main/java/nl/ru/cmbi/vase/web/page/HomePage.java
+++ src/main/java/nl/ru/cmbi/vase/web/page/HomePage.java
@@ -2,6 +2,7 @@ package nl.ru.cmbi.vase.web.page;
 
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -11,5 +12,13 @@ public class HomePage extends BasePage {
 
 	public HomePage(final PageParameters parameters) {
 
+		add(new BookmarkablePageLink("input-link",InputPage.class));
+		
+		String exampleID="1CRN";
+		PageParameters pp = new PageParameters();
+		pp.set(0, exampleID);
+		Link exampleLink = new BookmarkablePageLink("example-link",AlignmentPage.class,pp);
+		exampleLink.add(new Label("example-id",exampleID));
+		add(exampleLink);
     }
 }
